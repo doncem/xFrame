43e69ea7da4fda6aede4f9be8d1c3b7fa9008428
Donatas Martinkus <donatasmart@gmail.com>
2017-06-26 22:21:12 +0100

    adding event manager with table prefix event

--- lib/Xframe/Registry/DatabaseRegistry.php
+++ lib/Xframe/Registry/DatabaseRegistry.php
@@ -7,6 +7,7 @@ namespace Xframe\Registry;
  * @property string $PASSWORD
  * @property string $HOST
  * @property int    $PORT
+ * @property string $PREFIX
  * @property string $NAME
  * @property string $ENGINE
  *
@@ -18,6 +19,7 @@ final class DatabaseRegistry extends AbstractRegistry
     const PASSWORD = 'password';
     const HOST = 'host';
     const PORT = 3306;
+    const PREFIX = 'x';
     const NAME = 'name';
     const ENGINE = 'mysql';
 }
